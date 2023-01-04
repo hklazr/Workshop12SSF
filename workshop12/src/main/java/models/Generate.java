da1e5f9b6ce8521eebc5a688c223e660cfd46132
hklazr <haikelazharmohamed@gmail.com>
2023-01-04 17:00:33 +0800

    first commit

---
+++ workshop12/src/main/java/models/Generate.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Generate {
+        private int numberVal = 1;
+    
+        public int getNumberVal() {
+            return numberVal;
+        }
+    
+        public void setNumberVal(int numberVal) {
+            this.numberVal = numberVal;
+        }
+    
+    }
+
